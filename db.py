04445292bbca9f4758f674a0bd9a031a398e3d67
carsonbring <carsonbring@gmail.com>
2024-05-08 22:48:00 -0500

    EPS added (not TTM), MACD, MFI and sotchastic oscillator added to data collected

--- db.py
+++ db.py
@@ -6,6 +6,33 @@ import numpy as np
 import torch
 from torch.utils.data import TensorDataset, DataLoader
 import ta
+import requests
+from typing import Union
+
+
+def get_earnings_histo(symbol: str) -> Union[None, pd.DataFrame]:
+    symbol = symbol.upper()
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+
+    web_request = requests.get(url=f'https://finance.yahoo.com/calendar/earnings?symbol={symbol}', headers=headers)
+
+    try:
+        web_tables = pd.read_html(web_request.text)
+        results_df = web_tables[0]
+    except Exception as ex:
+        print(f"No earnings histo found for ticker: {symbol}")
+        print(f"Error is: {ex}")
+        return None
+    if len(results_df) == 0:
+        print(f"No earnings histo found for ticker: {symbol}")
+        return None
+    dates_df = results_df['Earnings Date'].str.split(',', expand=True, n=2)[[0, 1]]
+    results_df['earnings_date'] = dates_df[0].astype(str) + ' ' + dates_df[1].astype(str)
+    results_df['earnings_date'] = pd.to_datetime(results_df['earnings_date'])
+
+    return results_df
+
 
 def retrieve_data(stock_ticker, db_path='stocks.db'):
     # Connect to SQLite database
@@ -127,16 +154,36 @@ def update_stock_table(stock_ticker, db_path='stocks.db'):
     # Close the connection
     conn.close()
 
-def obtain_stock_data_yfinance(stock_ticker):
+def obtain_stock_data(stock_ticker):
+
     # Fetch the last 30 days of prices using Yahoo Finance
     end_date = datetime.now().strftime('%Y-%m-%d')
     start_date = (datetime.now() - timedelta(days=365)).strftime('%Y-%m-%d')
     df = yf.download(stock_ticker, start=start_date, end=end_date)
+    eps_df = get_earnings_histo(stock_ticker)
+    df.reset_index(inplace=True)
+    df['Date'] = pd.to_datetime(df['Date'])
+    if eps_df is not None:
+        eps_df['Date'] = pd.to_datetime(eps_df['earnings_date'])
+        eps_df = eps_df.sort_values('Date')
+        df['EPS'] = pd.merge_asof(df.sort_values('Date'), eps_df, on='Date', direction='backward')['Reported EPS']
     df['RSI'] = ta.momentum.RSIIndicator(df['Close'], window=14).rsi()
+
     indicator_bb = ta.volatility.BollingerBands(df['Close'], window=20, window_dev=2)
     df['BB_Middle'] = indicator_bb.bollinger_mavg()
     df['BB_Upper'] = indicator_bb.bollinger_hband()
     df['BB_Lower'] = indicator_bb.bollinger_lband()
+
+    indicator_macd = ta.trend.MACD(close=df['Close'], window_slow=26, window_fast=12, window_sign=9)
+    df['MACD'] = indicator_macd.macd()
+    df['MACD_Signal'] = indicator_macd.macd_signal()
+    df['MACD_Diff'] = indicator_macd.macd_diff()
+
+    df['MFI'] = ta.volume.MFIIndicator(df['High'], df['Low'], df['Close'], df['Volume'], window=14).money_flow_index()
+
+    indicator_so = ta.momentum.StochasticOscillator(high=df['High'], low=df['Low'], close=df['Close'], window=14, smooth_window=3)
+    df['Stock_Osc'] = indicator_so.stoch()
+    df['Stock_Osc_Signal'] = indicator_so.stoch_signal()
     return df
 
 
@@ -163,10 +210,17 @@ def create_stock_table_if_not_exists(stock_ticker, db_path='stocks.db'):
                 RSI REAL,
                 BB_Middle REAL,
                 BB_Upper REAL,
-                BB_Lower REAL
+                BB_Lower REAL,
+                MACD REAL,
+                MACD_Signal REAL,
+                MACD_Diff REAL,
+                MFI REAL,
+                Stoch_Osc REAL,
+                Stoch_Osc_Signal REAL
+                
             )
         """)
-        df = obtain_stock_data_yfinance(stock_ticker)
+        df = obtain_stock_data(stock_ticker)
 
         # Insert data into the table
         for index, row in df.iterrows():
@@ -195,4 +249,6 @@ pd.set_option('display.max_colwidth', None)
 # Optionally, set a large width for the terminal display to avoid wrapping
 pd.set_option('display.width', 1000)
 
-print(obtain_stock_data_yfinance('AAPL'))
+#print(obtain_stock_data_yfinance('AAPL'))
+
+print(obtain_stock_data('AAPL'))

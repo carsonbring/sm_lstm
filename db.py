afb42ba4a88fdfee516531e4c61bc1cda40da985
carsonbring <carsonbring@gmail.com>
2024-05-10 15:01:49 -0500

    changes

--- db.py
+++ db.py
@@ -5,9 +5,6 @@ import pandas as pd
 import numpy as np
 import torch
 from torch.utils.data import TensorDataset, DataLoader
-import ta
-import requests
-from typing import Union
 import scraper, utils
 
 
@@ -131,44 +128,6 @@ def update_stock_table(stock_ticker, db_path='stocks.db'):
     # Close the connection
     conn.close()
 
-def obtain_stock_data(stock_ticker):
-
-    # Fetch the last 30 days of prices using Yahoo Finance
-    end_date = datetime.now().strftime('%Y-%m-%d')
-    start_date = (datetime.now() - timedelta(days=1000)).strftime('%Y-%m-%d')
-    df = yf.download(stock_ticker, start=start_date, end=end_date)
-    eps_df = scraper.get_earnings_histo(stock_ticker)
-    df.reset_index(inplace=True)
-    df['Date'] = pd.to_datetime(df['Date'])
-    if eps_df is not None:
-
-        eps_df['Date'] = pd.to_datetime(eps_df['earnings_date'])
-        eps_df = eps_df.sort_values('Date')
-
-        eps_df = eps_df[eps_df['Reported EPS'] != '-']
-
-        df['EPS_TTM'] = df['EPS_TTM'] = df['Date'].apply(lambda x: utils.calculate_ttm_eps(x, eps_df))
-
-        df['PE_Ratio'] = df['Close'] / df['EPS_TTM']
-
-
-    indicator_bb = ta.volatility.BollingerBands(df['Close'], window=20, window_dev=2)
-    df['BB_Middle'] = indicator_bb.bollinger_mavg()
-    df['BB_Upper'] = indicator_bb.bollinger_hband()
-    df['BB_Lower'] = indicator_bb.bollinger_lband()
-
-    indicator_macd = ta.trend.MACD(close=df['Close'], window_slow=26, window_fast=12, window_sign=9)
-    df['MACD'] = indicator_macd.macd()
-    df['MACD_Signal'] = indicator_macd.macd_signal()
-    df['MACD_Diff'] = indicator_macd.macd_diff()
-
-    df['MFI'] = ta.volume.MFIIndicator(df['High'], df['Low'], df['Close'], df['Volume'], window=14).money_flow_index()
-
-    indicator_so = ta.momentum.StochasticOscillator(high=df['High'], low=df['Low'], close=df['Close'], window=14, smooth_window=3)
-    df['Stock_Osc'] = indicator_so.stoch()
-    df['Stock_Osc_Signal'] = indicator_so.stoch_signal()
-    return df
-
 
 def create_stock_table_if_not_exists(stock_ticker, db_path='stocks.db'):
     # Connect to SQLite database
@@ -202,7 +161,7 @@ def create_stock_table_if_not_exists(stock_ticker, db_path='stocks.db'):
                 
             )
         """)
-        df = obtain_stock_data(stock_ticker)
+        df = utils.obtain_stock_data(stock_ticker)
 
         # Insert data into the table
         for index, row in df.iterrows():
@@ -234,4 +193,4 @@ pd.set_option('display.width', 1000)
 #print(obtain_stock_data_yfinance('AAPL'))
 print(scraper.get_earnings_histo('AAPL'))
 print('----------------------------------------')
-print(obtain_stock_data('AAPL'))
+print(utils.obtain_stock_data('AAPL'))
--- utils.py
+++ utils.py
@@ -1,4 +1,9 @@
 import pandas as pd
+import scraper
+import yfinance as yf
+import ta
+from datetime import datetime, timedelta
+
 
 def calculate_ttm_eps(current_date, eps_data):
     # Filter reports that occurred before the current date
@@ -9,3 +14,41 @@ def calculate_ttm_eps(current_date, eps_data):
 
     # Sum their EPS values to get the TTM EPS
     return last_four_eps['Reported EPS'].sum()
+
+
+def obtain_stock_data(stock_ticker):
+    # Fetch the last 30 days of prices using Yahoo Finance
+    end_date = datetime.now().strftime('%Y-%m-%d')
+    start_date = (datetime.now() - timedelta(days=1000)).strftime('%Y-%m-%d')
+    df = yf.download(stock_ticker, start=start_date, end=end_date)
+    eps_df = scraper.get_earnings_histo(stock_ticker)
+    df.reset_index(inplace=True)
+    df['Date'] = pd.to_datetime(df['Date'])
+    if eps_df is not None:
+
+        eps_df['Date'] = pd.to_datetime(eps_df['earnings_date'])
+        eps_df = eps_df.sort_values('Date')
+
+        eps_df = eps_df[eps_df['Reported EPS'] != '-']
+
+        df['EPS_TTM'] = df['EPS_TTM'] = df['Date'].apply(lambda x: utils.calculate_ttm_eps(x, eps_df))
+
+        df['PE_Ratio'] = df['Close'] / df['EPS_TTM']
+
+    indicator_bb = ta.volatility.BollingerBands(df['Close'], window=20, window_dev=2)
+    df['BB_Middle'] = indicator_bb.bollinger_mavg()
+    df['BB_Upper'] = indicator_bb.bollinger_hband()
+    df['BB_Lower'] = indicator_bb.bollinger_lband()
+
+    indicator_macd = ta.trend.MACD(close=df['Close'], window_slow=26, window_fast=12, window_sign=9)
+    df['MACD'] = indicator_macd.macd()
+    df['MACD_Signal'] = indicator_macd.macd_signal()
+    df['MACD_Diff'] = indicator_macd.macd_diff()
+
+    df['MFI'] = ta.volume.MFIIndicator(df['High'], df['Low'], df['Close'], df['Volume'], window=14).money_flow_index()
+
+    indicator_so = ta.momentum.StochasticOscillator(high=df['High'], low=df['Low'], close=df['Close'], window=14, smooth_window=3)
+    df['Stock_Osc'] = indicator_so.stoch()
+    df['Stock_Osc_Signal'] = indicator_so.stoch_signal()
+    return df
+
